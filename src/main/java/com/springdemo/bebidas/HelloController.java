bbd4d1e3124e00265af97765abcb25e4a2b40ed4
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2024-05-22 21:14:05 -0600

    Añadido -> Ahora la console muestra un mensaje cada que presionamos un boton para confirmar que efectivamente funciona

--- src/main/java/com/springdemo/bebidas/HelloController.java
+++ src/main/java/com/springdemo/bebidas/HelloController.java
@@ -15,29 +15,29 @@ public class HelloController {
 
     public void onSaborSeleccionButtonClick(ActionEvent actionEvent) {
         if (actionEvent.getSource() == btnJamaica) {
-
+            System.out.println("jamaica");
         } else if (actionEvent.getSource() == btnLimon) {
-
+            System.out.println("limon");
         } else if (actionEvent.getSource() == btnHorchata) {
-
+            System.out.println("horchata");
         }
     }
 
     public void onTamanoSeleccionButtonClick(ActionEvent actionEvent) {
         if (actionEvent.getSource() == btnChico) {
-
+            System.out.println("chico");
         } else if (actionEvent.getSource() == btnMediano) {
-
+            System.out.println("mediano");
         } else if (actionEvent.getSource() == btnGrande) {
-
+            System.out.println("grande");
         }
     }
 
     public void onHieloSeleccionButtonClick(ActionEvent actionEvent) {
         if (actionEvent.getSource() == btnHieloSi) {
-
+            System.out.println("con hielo");
         } else if (actionEvent.getSource() == btnHieloNo) {
-
+            System.out.println("sin hielo");
         }
     }
 }
