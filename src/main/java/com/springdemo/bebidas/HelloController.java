697d6241f3df7bed4c901c4b1cee035f66588c2c
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2024-05-22 21:18:44 -0600

    Añadido -> setDisable a el resto de botones que no fueron seleccionados

--- src/main/java/com/springdemo/bebidas/HelloController.java
+++ src/main/java/com/springdemo/bebidas/HelloController.java
@@ -16,28 +16,42 @@ public class HelloController {
     public void onSaborSeleccionButtonClick(ActionEvent actionEvent) {
         if (actionEvent.getSource() == btnJamaica) {
             System.out.println("jamaica");
+            btnLimon.setDisable(true);
+            btnHorchata.setDisable(true);
         } else if (actionEvent.getSource() == btnLimon) {
             System.out.println("limon");
+            btnJamaica.setDisable(true);
+            btnHorchata.setDisable(true);
         } else if (actionEvent.getSource() == btnHorchata) {
             System.out.println("horchata");
+            btnJamaica.setDisable(true);
+            btnLimon.setDisable(true);
         }
     }
 
     public void onTamanoSeleccionButtonClick(ActionEvent actionEvent) {
         if (actionEvent.getSource() == btnChico) {
             System.out.println("chico");
+            btnMediano.setDisable(true);
+            btnGrande.setDisable(true);
         } else if (actionEvent.getSource() == btnMediano) {
             System.out.println("mediano");
+            btnChico.setDisable(true);
+            btnGrande.setDisable(true);
         } else if (actionEvent.getSource() == btnGrande) {
             System.out.println("grande");
+            btnChico.setDisable(true);
+            btnMediano.setDisable(true);
         }
     }
 
     public void onHieloSeleccionButtonClick(ActionEvent actionEvent) {
         if (actionEvent.getSource() == btnHieloSi) {
             System.out.println("con hielo");
+            btnHieloNo.setDisable(true);
         } else if (actionEvent.getSource() == btnHieloNo) {
             System.out.println("sin hielo");
+            btnHieloSi.setDisable(true);
         }
     }
 }
